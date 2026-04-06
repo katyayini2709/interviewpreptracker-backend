3bb97b052f70a7a4319c769a855926d7dfbeaa74
Katyayini Badda <baddakatyayini@gmail.com>
2026-04-06 23:17:47 +0530

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const driver = neo4j.driver(
     "neo4j+s://483b1eb8.databases.neo4j.io",
-    neo4j.auth.basic("483b1eb8", "cDR7fw2WZIz0wq6kuz5dm2DwP6qCywW2m6CrQtM")
+    neo4j.auth.basic("neo4j", "cDR7fw2WZIz0wq6kuz5dm2DwP6qCywW2tjW2m6CrQtM")
 );
 
 const PORT = process.env.PORT || 5000;
